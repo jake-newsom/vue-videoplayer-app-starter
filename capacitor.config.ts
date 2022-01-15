afe7600a8610eb27fb607a65f872e270b755bfa2
jepiqueau <jepi.queau@free.fr>
2022-01-15 18:33:46 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'vue-videoplayer-app-starter',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
